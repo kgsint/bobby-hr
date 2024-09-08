34b682347c1a8b4a6a85c77ea5db80eb74154fe2
Kaung Sint Ko Ko <kaungsintkoko99@gmail.com>
2024-09-09 01:49:07 +0630

    rename session table

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,12 @@ Bundler.require(*Rails.groups)
 
 module BobbyHr1
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Yangon"
+    # session name
+    config.session_store :cookie_store, key: "bobby_hr_sess"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
